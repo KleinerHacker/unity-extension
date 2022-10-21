bee721d8bb59627886488164e7dc3a0ec85e6f8e
KleinerHacker <Christoph.Pfeiffer.Berlin@freenet.de>
2022-10-21 19:53:44 +0200

    insert extension methods

---
+++ Scripts/Runtime/Utils/Extensions/CameraExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace UnityExtension.Runtime.extension.Scripts.Runtime.Utils.Extensions
+{
+    public static class CameraExtensions
+    {
+        public static Bounds GetOrthographicBounds(this Camera camera)
+        {
+            if (!camera.orthographic)
+                throw new InvalidOperationException("Camera is not orthographic: " + camera.gameObject.name);
+            
+            var screenAspect = (float)Screen.width / (float)Screen.height;
+            var cameraHeight = camera.orthographicSize * 2;
+            var bounds = new Bounds(
+                camera.transform.position,
+                new Vector3(cameraHeight * screenAspect, cameraHeight, 0));
+            
+            return bounds;
+        }
+    }
+}
