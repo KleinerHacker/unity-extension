ea50b8c1afa91833f841615faac456408e263a1b
KleinerHacker <Christoph.Pfeiffer.Berlin@freenet.de>
2022-11-13 20:37:49 +0100

    insert gizmos notes, insert terrain splitter by Dvornik

---
+++ Scripts/Runtime/Utils/Extensions/RectTransformExtensions.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace UnityExtension.Runtime.extension.Scripts.Runtime.Utils.Extensions
+{
+    public static class RectTransformExtensions
+    {
+        public static Rect CalculateAbsoluteRect(this RectTransform transform, Canvas canvas)
+        {
+            var position = transform.position;
+            var x = position.x;
+            var y = position.y;
+            
+            var rect = transform.rect;
+            var width = rect.width;
+            var height = rect.height;
+
+            return new Rect(x, y, width, height);
+        }
+    }
+}
